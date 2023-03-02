c0d4c71931e85a6eb897f0c9bfef16d2593df746
test <test>
2023-03-02 22:51:44 +0700

    test

---
+++ faces.py
@@ -0,0 +1,15 @@
+def main():
+    x = input()
+    print(convert(x))
+
+def convert(x):
+    return x.replace(":)", '\N{Slightly Smiling Face}')
+
+def main():
+    d = input()
+    print(convert(d))
+
+def convert(d):
+    return d.replace(":(", '\N{Slightly Frowning Face}')
+
+main()
